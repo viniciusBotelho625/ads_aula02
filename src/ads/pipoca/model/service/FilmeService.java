44251b11e50f0dd17e6f050dada5887f03452be0
viniciusBotelho625 <58275652+viniciusBotelho625@users.noreply.github.com>
2020-03-22 18:47:48 -0300

    Fazendo a ponte entre Service e DAO

--- src/ads/pipoca/model/service/FilmeService.java
+++ src/ads/pipoca/model/service/FilmeService.java
@@ -1,6 +1,7 @@
 package ads.pipoca.model.service;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 import ads.pipoca.model.dao.FilmeDAO;
 import ads.pipoca.model.entity.Filme;
@@ -15,5 +16,18 @@ public class FilmeService {
 	public Filme buscarFilme(int id) throws IOException {
 		FilmeDAO dao = new FilmeDAO();
 		return dao.buscarFilme(id);
+	}
+	
+	public void excluirFilme(int id) throws IOException {
+		FilmeDAO dao = new FilmeDAO();
+		dao.excluirFilme(id);
+	}
+	public String alterarFilme(Filme filme) throws IOException {
+		FilmeDAO dao = new FilmeDAO();
+		return dao.alterarFilme(filme);
+	}
+	public ArrayList<Filme> listarFilmes() throws IOException {
+		FilmeDAO dao = new FilmeDAO();
+		return dao.listarFilmes();
 	}
 }
